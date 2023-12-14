5da5ff9805be3bf534df1e3ea88378c8a0621f56
PoliYag <150130450+PoliYag@users.noreply.github.com>
2023-12-14 18:20:04 +0300

    задание 2 урок 7

---
+++ task8_2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Program
+{
+    public static int AckermannFunction(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (n == 0)
+        {
+            return AckermannFunction(m - 1, 1);
+        }
+        else
+        {
+            return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+        }
+    }
+    
+    public static void Main()
+    {
+        int m = 2;
+        int n = 1;
+        
+        int result = AckermannFunction(m, n);
+        
+        Console.WriteLine(result);
+    }
+}
